e19e78e17315a15a91277a466a9bc5a1ff7f8c6e
Gagan021-5 <gagansingh010221@gmail.com>
2025-06-17 01:38:35 +0530

    another fix

--- back/index.js
+++ back/index.js
@@ -5,19 +5,19 @@ import cors from "cors";
 import "dotenv/config";
 import bodyParser from "body-parser";
 
-app.use(express.static("public"));       //for converting thh object into the actual words for the generated text
+app.use(express.static("public")); //for converting thh object into the actual words for the generated text
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(cors({
-    origin: "https://moviesfront-q1wi.onrender.com",
-    credentials:true
-}))
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://moviesfront-q1wi.onrender.com/"],
+    credentials: true,
+  })
+);
 
 //router
 import router from "./router/ai.js";
 app.use("/", router);
-
-
 
 app.listen(PORT, () => {
   console.log(`Port is listening at ${PORT}`);
